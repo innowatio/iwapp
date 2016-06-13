a1ad07ab9643a78e88b0f54df9e6aab0cc503e48
Davide Bianchi <davide.bianchi@mondora.com>
2016-06-13 17:48:47 +0200

    Fixed require of iwwa-icons

--- test/unit/components/side-menu.js
+++ test/unit/components/side-menu.js
@@ -3,7 +3,7 @@ import {shallow} from "enzyme";
 import SideMenu from "components/side-menu";
 import DropDown from "components/drop-down";
 import Text from "components/text-lato";
-import Icon from "components/iwwa-icons";
+import Icon from "components/iwapp-icons";
 
 describe("`SideMenu` component", () => {
 
@@ -15,22 +15,22 @@ describe("`SideMenu` component", () => {
         expect(dropDown.find(Text)).to.have.length(7);
 
         expect(dropDown.find(Text).get(0).props.children).to.be.equal("Overview");
-        expect(dropDown.find(Icon).get(0).props.name).to.be.equal("iw-dashboard");
+        expect(dropDown.find(Icon).get(0).props.name).to.be.equal("iw-overview");
 
         expect(dropDown.find(Text).get(1).props.children).to.be.equal("Statistiche");
-        expect(dropDown.find(Icon).get(1).props.name).to.be.equal("iw-chart-style1");
+        expect(dropDown.find(Icon).get(1).props.name).to.be.equal("iw-chart");
 
         expect(dropDown.find(Text).get(2).props.children).to.be.equal("Il mio smartmeter");
         expect(dropDown.find(Icon).get(2).props.name).to.be.equal("iw-gauge");
 
         expect(dropDown.find(Text).get(3).props.children).to.be.equal("Badgeboard");
-        expect(dropDown.find(Icon).get(3).props.name).to.be.equal("iw-assign");
+        expect(dropDown.find(Icon).get(3).props.name).to.be.equal("iw-badge");
 
         expect(dropDown.find(Text).get(4).props.children).to.be.equal("Risparmio energetico");
         expect(dropDown.find(Icon).get(4).props.name).to.be.equal("iw-lightbulb");
 
         expect(dropDown.find(Text).get(5).props.children).to.be.equal("Report");
-        expect(dropDown.find(Icon).get(5).props.name).to.be.equal("iw-info");
+        expect(dropDown.find(Icon).get(5).props.name).to.be.equal("iw-report");
 
         expect(dropDown.find(Text).get(6).props.children).to.be.equal("Logout");
         expect(dropDown.find(Icon).get(6).props.name).to.be.equal("iw-logout");
