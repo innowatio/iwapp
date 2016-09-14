a24fefa41bac7606f8851aac8268698069e96910
francesca <francesca.giudici@mondora.com>
2016-09-14 16:34:50 +0200

    Added a function on notifications text, that on click show the full message of the notification.

--- app/views/notifications.js
+++ app/views/notifications.js
@@ -1,7 +1,7 @@
 import {Map} from "immutable";
 import {equals} from "ramda";
 import React, {Component, PropTypes} from "react";
-import {Dimensions, StyleSheet, ListView, View} from "react-native";
+import {Dimensions, TouchableHighlight, StyleSheet, ListView, View} from "react-native";
 import {Content} from "native-base";
 import {connect} from "react-redux";
 import IPropTypes from "react-immutable-proptypes";
@@ -39,29 +39,37 @@ const styles = StyleSheet.create({
     },
     notificationsContainer: {
         marginVertical: 20,
-        justifyContent: "space-around",
+        justifyContent: "center",
         alignItems: "center"
     },
     notificationWrp: {
         flexDirection: "row",
-        alignItems: "center",
-        marginVertical: 10
+        alignItems: "flex-start",
+        justifyContent: "center",
+        marginVertical: 8
     },
     notificationIcon: {
         borderRadius: 100,
-        width: 50,
-        height: 50,
         justifyContent: "center",
         alignItems: "center"
+    },
+    notificationTextWrp: {
+        justifyContent: "center",
+        alignItems: "flex-start"
     },
     notificationText: {
         color: colors.textGrey,
         fontSize: 13
     },
+    notificationDateWrp: {
+        justifyContent: "flex-end",
+        alignItems: "flex-end"
+    },
     notificationDate: {
+        alignSelf: "flex-end",
         color: colors.lightGrey,
-        fontSize: 13,
-        alignSelf: "flex-end"
+        fontSize: 10,
+        textAlign: "right"
     }
 });
 
@@ -75,7 +83,15 @@ class Notifications extends Component {
     constructor (props) {
         super(props);
         const dataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => !equals(r1, r2)});
-        this.state = {dataSource};
+        this.state = {
+            dataSource
+        };
+    }
+
+    componentWillMount () {
+        this.setState({
+            dataSource: this.state.dataSource.cloneWithRows(this.getNotifications())
+        });
     }
 
     componentDidMount () {
@@ -85,6 +101,13 @@ class Notifications extends Component {
     componentWillReceiveProps (nextProps) {
         this.setState({
             dataSource: this.state.dataSource.cloneWithRows(this.getNotifications(nextProps))
+        });
+    }
+
+    pressRow (rowID) {
+        const notifications = this.getNotifications(this.props, rowID);
+        this.setState({
+            dataSource: this.state.dataSource.cloneWithRows(notifications),
         });
     }
 
@@ -114,55 +137,97 @@ class Notifications extends Component {
         }
     }
 
-    getNotifications (nextProps) {
+    getNotificationText (notification, index, rowID) {
+        if (rowID === index) {
+            return (
+                <Text style={styles.notificationText}>
+                    {notification.get("message")} {"ndua inv iaon vaiovn aio mcoianoa no nion in ioni n ion in ndua inv iaon vaiovn aio mcoianoa no nion in ioni fine."}
+                </Text>
+            );
+        } else {
+            return (
+                <Text
+                    ellipsizeMode={"tail"}
+                    numberOfLines={2}
+                    style={styles.notificationText}
+                >
+                    {notification.get("message")} {"ndua inv iaon vaiovn aio mcoianoa no nion in ioni n ion in ndua inv iaon vaiovn aio mcoianoa no nion in ioni fine."}
+                </Text>
+            );
+        }
+    }
+
+    // text: this.getNotificationText(notification, index, this.state.pressed),
+
+    getNotifications (nextProps, rowID) {
         const props = nextProps ? nextProps : this.props;
         const notifs = props.collections.get("notifications") || Map();
-        return notifs.map((notification, index) => {
+        console.log("rowID");
+        console.log(rowID);
+        return notifs.sortBy(notification => -notification.date).toArray().map((notification, index) => {
             const type = notification.get("type");
             return {
                 key: index,
-                text: notification.get("message"),
+                text: this.getNotificationText(notification, index, parseInt(rowID)),
                 icon: this.getNotificationIcon(type),
                 bgcolor: this.getNotificationBackgroundColor(type),
                 date: notification.get("date")
             };
-        }).sortBy(notification => -notification.date).toArray();
+        });
     }
 
-    renderNotificationsList () {
-        return (
-            <ListView
-                dataSource={this.state.dataSource}
-                renderRow={this.renderNotification}
-                style={styles.containerView}
-            />
-        );
-    }
-
-    renderNotification (notification) {
+    renderNotification (notification, rowID, index) {
         const {width} = Dimensions.get("window");
         const date = new Date(notification.date);
+        console.log("ROWIDIDIDIDI");
+        console.log(index);
         return (
-            <View key={notification.key} style={styles.notificationWrp}>
-                <View style={{width: width * 0.16}}>
-                    <View style={[styles.notificationIcon, {backgroundColor: notification.bgcolor}]}>
+            <View key={notification.key} style={[styles.notificationWrp, {width: width * .9}]}>
+                <View style={{width: width * .17}}>
+                    <View style={[styles.notificationIcon, {
+                        width: width * .14,
+                        height: width * .14,
+                        backgroundColor: notification.bgcolor}
+                    ]}>
                         <Icon
                             color={colors.iconWhite}
                             name={notification.icon}
-                            size={40}
+                            size={width * .10}
                             style={{backgroundColor: colors.transparent}}
                         />
                     </View>
                 </View>
-                <View style={{width: width * 0.74}}>
-                    <Text style={styles.notificationText}>
-                        {notification.text}
-                    </Text>
-                    <Text style={styles.notificationDate}>
-                        {moment.utc(date).format("ddd DD MMM")}
-                    </Text>
-                </View>
+                <TouchableHighlight
+                    onPress={() => {
+                        ::this.pressRow(index);
+                    }}
+                    underlayColor={colors.white}
+                >
+                    <View style={{flexDirection: "column", width: width * .73}}>
+                        <View style={[
+                            styles.notificationTextWrp,
+                            {minHeight: width * .12, width: width * .7}
+                        ]}>
+                            {notification.text}
+                        </View>
+                        <View style={[styles.notificationDateWrp]}>
+                            <Text style={styles.notificationDate}>
+                                {moment.utc(date).format("ddd DD MMM")}
+                            </Text>
+                        </View>
+                    </View>
+                </TouchableHighlight>
             </View>
+        );
+    }
+    renderNotificationsList () {
+        return (
+            <ListView
+                dataSource={this.state.dataSource}
+                enableEmptySections={true}
+                renderRow={::this.renderNotification}
+                style={styles.containerView}
+            />
         );
     }
 
